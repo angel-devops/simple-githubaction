6f81d6eb0bec62ba7fce48f76c5ac01933665180
angel <angel@angel.com>
2021-01-27 15:04:53 -0500

    adding complex job v1

---
+++ fruits/fruit.py
@@ -0,0 +1,7 @@
+class Fruit():
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+    
+    def show_message():
+        return "{0} has a cost of ${1} ".format(self.name, self.price)
